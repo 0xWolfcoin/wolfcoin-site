c257972781fbad1dbda1136f72efea1faf26acf3
0xWolfcoin <101480523+0xWolfcoin@users.noreply.github.com>
2022-07-12 17:10:00 -0400

    Fixed Image Optimization error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: { // https://stackoverflow.com/a/70047180
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
 };
